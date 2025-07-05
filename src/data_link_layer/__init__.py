81b9837a7bab2f2bf9da00d8a7284d2f04c1039c
Miguel-MCM <miguelcarvalhodemedeiros.mcm@gmail.com>
2025-07-05 15:17:57 -0300

    other layers

---
+++ src/data_link_layer/__init__.py
@@ -0,0 +1,8 @@
+from .byte_flag_framer import ByteFlagFramer
+from .bits_flag_framer import BitsFlagFramer
+from .char_counting_framer import CharCountingFramer
+
+from .parity_error_detector import ParityErrorDetector
+from .crc_error_detector import CRCErrorDetector
+
+__other__ = ['ByteFlagFramer', 'BitsFlagFramer', 'CharCountingFramer', 'ParityErrorDetector', 'CRCErrorDetector']
--- src/physical_layer/__init__.py
+++ src/physical_layer/__init__.py
@@ -1,3 +1,6 @@
 from .digital_modulator import *
+from .bipolar_modulator import *
+from .manchester_modulator import *
+from .nrz_modulator import *
 
-__other__ = ['DigitalModulator']
+__other__ = ['DigitalModulator', 'BipolarModulator', 'ManchesterModulator', 'NRZModulator']
